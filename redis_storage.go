f525b58a8a59b51a3caa83b926886ca8a05ccdbd
Achilleas Anagnostopoulos <achilleas@taxibeat.com>
2015-07-01 08:23:26 +0300

    Redis storage implementation

---
+++ redis_storage.go
@@ -0,0 +1,165 @@
+package trace
+
+import (
+	"time"
+
+	"encoding/json"
+	"fmt"
+
+	"sort"
+
+	"github.com/achilleasa/usrv/middleware"
+	"github.com/garyburd/redigo/redis"
+)
+
+type Redis struct {
+	connPool *redis.Pool
+}
+
+func NewRedisStorage(redisEndpoint string, password string, db uint, timeout time.Duration) *Redis {
+	return &Redis{
+		connPool: &redis.Pool{
+			MaxIdle:     3,
+			IdleTimeout: 240 * time.Second,
+			Dial: func() (redis.Conn, error) {
+				c, err := redis.DialTimeout("tcp", redisEndpoint, timeout, timeout, timeout)
+				if err != nil {
+					return nil, err
+				}
+				if password != "" {
+					if _, err = c.Do("AUTH", password); err != nil {
+						c.Close()
+						return nil, err
+					}
+				}
+				if db > 0 {
+					if _, err = c.Do("SELECT", db); err != nil {
+						c.Close()
+						return nil, err
+					}
+				}
+
+				return c, err
+			},
+			TestOnBorrow: func(c redis.Conn, t time.Time) error {
+				_, err := c.Do("PING")
+				return err
+			},
+		},
+	}
+}
+
+// Store a trace entry and set a TTL on it. If the ttl is 0 then the
+// trace record will never expire. Implements the Storage interface.
+func (r *Redis) Store(logEntry *middleware.TraceEntry, ttl time.Duration) error {
+	json, err := json.Marshal(logEntry)
+	if err != nil {
+		return err
+	}
+
+	conn := r.connPool.Get()
+	defer conn.Close()
+
+	conn.Send("MULTI")
+
+	// Append log entry to a list that shares the same traceId
+	// and set a TTL
+	traceKey := fmt.Sprintf("trace.%s", logEntry.TraceId)
+	conn.Send("LPUSH", traceKey, json)
+	if ttl > time.Second {
+		conn.Send("EXPIRE", traceKey, ttl.Seconds())
+	}
+
+	// Add logEntry.From to the set of known services
+	conn.Send("SADD", "trace.services", logEntry.From)
+
+	// If this is an outgoing request, add the destination to the dependency set
+	// for the origin
+	if logEntry.Type == middleware.Request {
+		conn.Send("SADD", fmt.Sprintf("trace.%s.deps", logEntry.From), logEntry.To)
+	}
+
+	// Exec pipeline
+	_, err = conn.Do("EXEC")
+	return err
+}
+
+// Fetch a set of time-ordered trace entries with the given trace-id
+func (r *Redis) GetTrace(traceId string) (middleware.Trace, error) {
+
+	conn := r.connPool.Get()
+	defer conn.Close()
+
+	// Get the number of records
+	traceKey := fmt.Sprintf("trace.%s", traceId)
+	len, err := redis.Int(conn.Do("LLEN", traceKey))
+	if err != nil {
+		return nil, err
+	}
+
+	// Fetch all records
+	rawRows, err := redis.Strings(conn.Do("LRANGE", traceKey, 0, len))
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal raw data
+	trace := make(middleware.Trace, len)
+	for index, rawRow := range rawRows {
+		entry := middleware.TraceEntry{}
+		err = json.Unmarshal([]byte(rawRow), &entry)
+		if err != nil {
+			return nil, err
+		}
+		trace[index] = entry
+	}
+
+	// Sort the trace so entries appear in insertion order
+	sort.Sort(trace)
+
+	return trace, nil
+}
+
+// Get service dependencies optionally filtered by a set of service names. If no filters are
+// specified then the response will include all services currently known to the storage.
+func (r *Redis) GetDependencies(srvFilter ...string) ([]ServiceDependencies, error) {
+	conn := r.connPool.Get()
+	defer conn.Close()
+
+	var err error
+
+	if len(srvFilter) == 0 {
+		srvFilter, err = redis.Strings(conn.Do("SMEMBERS", "trace.services"))
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Fetch deps in a single batch
+	conn.Send("MULTI")
+	for _, serviceName := range srvFilter {
+		conn.Send("SMEMBERS", fmt.Sprintf("trace.%s.deps", serviceName))
+	}
+	replies, err := redis.Values(conn.Do("EXEC"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Assemble deps
+	replyCount := len(srvFilter)
+	serviceDeps := make([]ServiceDependencies, replyCount)
+	for index := 0; index < replyCount; index++ {
+		deps, _ := redis.Strings(replies[index], nil)
+		serviceDeps[index] = ServiceDependencies{
+			Service:      srvFilter[index],
+			Dependencies: deps,
+		}
+	}
+
+	return serviceDeps, nil
+}
+
+// Shutdown the storage.
+func (r *Redis) Close() error {
+	return r.connPool.Close()
+}
---
+++ redis_storage_test.go
@@ -0,0 +1,109 @@
+package trace
+
+import (
+	"testing"
+	"time"
+
+	"os"
+
+	"reflect"
+
+	"sort"
+
+	"github.com/achilleasa/usrv/middleware"
+)
+
+var (
+	redisEndpoint = ":6379"
+)
+
+func init() {
+	// Allow test runner to override redis host via the REDIS_HOST env var
+	opt := os.Getenv("REDIS_HOST")
+	if opt != "" {
+		redisEndpoint = opt
+	}
+}
+
+func TestRedisStorage(t *testing.T) {
+	storage := NewRedisStorage(redisEndpoint, "", 1, time.Second*10)
+	defer storage.Close()
+
+	// flush db
+	_, err := storage.connPool.Get().Do("FLUSHDB")
+	if err != nil {
+		t.Fatalf("Error flushing redis db: %v", err)
+	}
+
+	now := time.Now()
+	traceId := "abcd-1234-1234-1234"
+
+	// Shuffled records to simulate appends by different processes
+	dataSet := middleware.Trace{
+		middleware.TraceEntry{Type: middleware.Response, From: "com.service3", To: "com.service2", Timestamp: now.Add(time.Second * 3), TraceId: traceId},
+		middleware.TraceEntry{Type: middleware.Request, From: "com.service2", To: "com.service3", Timestamp: now.Add(time.Second * 2), TraceId: traceId},
+		middleware.TraceEntry{Type: middleware.Response, From: "com.service2", To: "com.service1", Timestamp: now.Add(time.Second * 4), TraceId: traceId},
+		middleware.TraceEntry{Type: middleware.Request, From: "com.service1", To: "com.service2", Timestamp: now.Add(time.Second * 1), TraceId: traceId},
+	}
+
+	// Generate the final sorted set that we will use for comparisons
+	sortedDataSet := make(middleware.Trace, len(dataSet))
+	copy(sortedDataSet, dataSet)
+	sort.Sort(sortedDataSet)
+
+	// Insert trace
+	ttl := time.Minute
+	for index, entry := range dataSet {
+		err := storage.Store(&entry, ttl)
+		if err != nil {
+			t.Fatalf("Error while storing entry #%d: %v", index, err)
+		}
+	}
+
+	// Fetch trace by id
+	trace, err := storage.GetTrace(traceId)
+	if err != nil {
+		t.Fatalf("Error retrieving trace: %v", err)
+	}
+
+	if !reflect.DeepEqual(sortedDataSet, trace) {
+		t.Fatalf("Expected retrieved trace to be equal to %v; got %v", sortedDataSet, trace)
+	}
+
+	// Get dependencies
+	depTests := []ServiceDependencies{
+		ServiceDependencies{Service: "com.service1", Dependencies: []string{"com.service2"}},
+		ServiceDependencies{Service: "com.service2", Dependencies: []string{"com.service3"}},
+		ServiceDependencies{Service: "com.service3", Dependencies: []string{}},
+	}
+
+	// Fetch using filters
+	for index, depSpec := range depTests {
+		deps, err := storage.GetDependencies(depSpec.Service)
+		if err != nil {
+			t.Fatalf("Error retrieving dep set #%d: %v", index, err)
+		}
+		if len(deps) != 1 {
+			t.Fatalf("Expected retrieved dependencies for set #%d to have length 1; got %d", index, len(deps))
+		}
+		dep := deps[0]
+		if dep.Service != depSpec.Service {
+			t.Fatalf("Expected dependency set #%d to contain dependencies for %s; got %s", index, depSpec.Service, dep.Service)
+		}
+		if !reflect.DeepEqual(depSpec.Dependencies, dep.Dependencies) {
+			t.Fatalf("Expected dependency set #%d to contain dependencies %v; got %v", index, depSpec.Dependencies, dep.Dependencies)
+		}
+	}
+
+	// Fetch all
+	deps, err := storage.GetDependencies()
+	if err != nil {
+		t.Fatalf("Error retrieving dependencies: %v", err)
+	}
+	if len(depTests) != len(deps) {
+		t.Fatalf("Expected retrieved dependencies to have length %d; got %d", len(depTests), len(deps))
+	}
+	if !reflect.DeepEqual(depTests, deps) {
+		t.Fatalf("Expected dependency set %v; got %v", depTests, deps)
+	}
+}
